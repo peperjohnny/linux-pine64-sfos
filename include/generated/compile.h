63ffc1009c7a0e130ca56da29b118d993149c3a4
peperjohnny <github@peperjohnny.org>
2020-10-17 11:18:21 +0200

    Kernel from Arch Linux ARM patched and ready to for Sailfish OS

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP Sat Oct 17 09:28:34 CEST 2020"
+#define LINUX_COMPILE_BY "johnny"
+#define LINUX_COMPILE_HOST "johnnito"
+#define LINUX_COMPILER "aarch64-linux-gnu-gcc (GCC) 10.2.0, GNU ld (GNU Binutils) 2.35.1"
